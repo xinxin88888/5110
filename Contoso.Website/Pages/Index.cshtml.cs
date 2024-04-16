210530b00e317be1a30d5d8ba269752eb1a053f0
Xinxin <isinsin267@gmail.com>
2024-04-16 10:56:51 -0700

    Initial commit

---
+++ Contoso.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Contoso.Website.Models;
+using Contoso.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Contoso.Website.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    public JsonFileProductService ProductService;
+    public IEnumerable<Product> Products { get; private set; }
+    public IndexModel(ILogger<IndexModel> logger,
+        JsonFileProductService productService)
+    {
+        _logger = logger;
+        ProductService = productService;
+    }
+
+    public void OnGet()
+    {
+        Products = ProductService.GetProducts();
+    }
+}
+
